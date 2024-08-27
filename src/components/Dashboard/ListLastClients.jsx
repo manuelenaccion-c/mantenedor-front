11c0613286d7f46dcdc6a7f5f2c3e79f9d0330a7
Manuel Salazar <manuel.salazar@enaccion-c.cl>
2024-08-27 16:39:26 -0400

    reverse

--- src/components/Dashboard/ListLastClients.jsx
+++ src/components/Dashboard/ListLastClients.jsx
@@ -3,9 +3,8 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { IconButton, Tooltip, useMediaQuery } from '@mui/material';
+import { useMediaQuery } from '@mui/material';
 import { newDate } from '../../utils/utils';
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 
 
 export default function ListLastClient({ recent_clients }) {
@@ -28,12 +27,7 @@ export default function ListLastClient({ recent_clients }) {
                 {recent_clients.length > 0 ?
                     recent_clients.map((client, id) => (
                         <Typography key={id} sx={{ borderBottom: "1px solid #cacaca", padding: 2, display: 'flex', justifyContent: 'space-between' }} variant="body2" color="text.secondary">
-                            {client.name} {client.last_name}  <span></span>
-                            <Tooltip title={newDate(client.created_at)} sx={{ padding: 0 }} placement="left">
-                                <IconButton>
-                                    <CalendarTodayIcon sx={{ fontSize: '1rem', color: '#282A3A' }} />
-                                </IconButton>
-                            </Tooltip>
+                            {client.name} {client.last_name}  <span>{newDate(client.created_at)}</span>
                         </Typography>
                     )) :
                     <Typography sx={{ borderBottom: "1px solid #cacaca", padding: 2, }} variant="body2" color="text.secondary">
